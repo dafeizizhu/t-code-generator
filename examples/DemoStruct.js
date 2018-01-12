c4b2185871c58664515f2335f581f1717b2642f0
maizhiying <zhiying.mai@gmail.com>
2018-01-12 15:23:37 +0800

    rename

--- examples/DemoStruct.js
+++ examples/DemoStruct.js
@@ -1,7 +1,7 @@
-const TStream = require('t-model')
+const TModel = require('t-model')
 const md5 = require('md5')
 
-class DemoStruct extends TStream.TStruct {
+class DemoStruct extends TModel.TStruct {
   static parse (value) {
     return Object.assign({}, {
       intValue: 0,
@@ -20,19 +20,19 @@ class DemoStruct extends TStream.TStruct {
     }, value)
   }
   static readFrom (is, tBuffer) {
-    let intValue = (TStream.TInt32).read(is, 0, true)
-    let stringValue = (TStream.TString).read(is, 1, true)
-    let floatValue = (TStream.TFloat).read(is, 2, true)
-    let doubleValue = (TStream.TDouble).read(is, 3, true)
-    let listValue = (TStream.TList(TStream.TString)).read(is, 4, false)
-    let mapValue = (TStream.TMap(TStream.TInt32, TStream.TString)).read(is, 5, false)
-    let charValue = (TStream.TInt8).read(is, 6, true)
-    let shortValue = (TStream.TInt16).read(is, 7, true)
-    let longlongValue = (TStream.TInt64).read(is, 8, true)
-    let ucharValue = (TStream.TUInt8).read(is, 9, true)
-    let ushortValue = (TStream.TUInt16).read(is, 10, true)
-    let uintValue = (TStream.TUInt32).read(is, 11, true)
-    let bytesValue = (TStream.TBytes).read(is, 12, true)
+    let intValue = (TModel.TInt32).read(is, 0, true)
+    let stringValue = (TModel.TString).read(is, 1, true)
+    let floatValue = (TModel.TFloat).read(is, 2, true)
+    let doubleValue = (TModel.TDouble).read(is, 3, true)
+    let listValue = (TModel.TList(TModel.TString)).read(is, 4, false)
+    let mapValue = (TModel.TMap(TModel.TInt32, TModel.TString)).read(is, 5, false)
+    let charValue = (TModel.TInt8).read(is, 6, true)
+    let shortValue = (TModel.TInt16).read(is, 7, true)
+    let longlongValue = (TModel.TInt64).read(is, 8, true)
+    let ucharValue = (TModel.TUInt8).read(is, 9, true)
+    let ushortValue = (TModel.TUInt16).read(is, 10, true)
+    let uintValue = (TModel.TUInt32).read(is, 11, true)
+    let bytesValue = (TModel.TBytes).read(is, 12, true)
     return new DemoStruct({
       intValue, stringValue, floatValue, doubleValue, listValue, mapValue, charValue, shortValue, longlongValue, ucharValue, ushortValue, uintValue, bytesValue
     }).valueOf()
@@ -40,19 +40,19 @@ class DemoStruct extends TStream.TStruct {
   constructor (value) {
     super(value)
 
-    this._t_intValue = new (TStream.TInt32)(this._value.intValue)
-    this._t_stringValue = new (TStream.TString)(this._value.stringValue)
-    this._t_floatValue = new (TStream.TFloat)(this._value.floatValue)
-    this._t_doubleValue = new (TStream.TDouble)(this._value.doubleValue)
-    this._t_listValue = new (TStream.TList(TStream.TString))(this._value.listValue)
-    this._t_mapValue = new (TStream.TMap(TStream.TInt32, TStream.TString))(this._value.mapValue)
-    this._t_charValue = new (TStream.TInt8)(this._value.charValue)
-    this._t_shortValue = new (TStream.TInt16)(this._value.shortValue)
-    this._t_longlongValue = new (TStream.TInt64)(this._value.longlongValue)
-    this._t_ucharValue = new (TStream.TUInt8)(this._value.ucharValue)
-    this._t_ushortValue = new (TStream.TUInt16)(this._value.ushortValue)
-    this._t_uintValue = new (TStream.TUInt32)(this._value.uintValue)
-    this._t_bytesValue = new (TStream.TBytes)(this._value.bytesValue)
+    this._t_intValue = new (TModel.TInt32)(this._value.intValue)
+    this._t_stringValue = new (TModel.TString)(this._value.stringValue)
+    this._t_floatValue = new (TModel.TFloat)(this._value.floatValue)
+    this._t_doubleValue = new (TModel.TDouble)(this._value.doubleValue)
+    this._t_listValue = new (TModel.TList(TModel.TString))(this._value.listValue)
+    this._t_mapValue = new (TModel.TMap(TModel.TInt32, TModel.TString))(this._value.mapValue)
+    this._t_charValue = new (TModel.TInt8)(this._value.charValue)
+    this._t_shortValue = new (TModel.TInt16)(this._value.shortValue)
+    this._t_longlongValue = new (TModel.TInt64)(this._value.longlongValue)
+    this._t_ucharValue = new (TModel.TUInt8)(this._value.ucharValue)
+    this._t_ushortValue = new (TModel.TUInt16)(this._value.ushortValue)
+    this._t_uintValue = new (TModel.TUInt32)(this._value.uintValue)
+    this._t_bytesValue = new (TModel.TBytes)(this._value.bytesValue)
   }
 
   get intValue () {
@@ -96,43 +96,43 @@ class DemoStruct extends TStream.TStruct {
   }
 
   set intValue (value) {
-    this._t_intValue = new (TStream.TInt32)(value)
+    this._t_intValue = new (TModel.TInt32)(value)
   }
   set stringValue (value) {
-    this._t_stringValue = new (TStream.TString)(value)
+    this._t_stringValue = new (TModel.TString)(value)
   }
   set floatValue (value) {
-    this._t_floatValue = new (TStream.TFloat)(value)
+    this._t_floatValue = new (TModel.TFloat)(value)
   }
   set doubleValue (value) {
-    this._t_doubleValue = new (TStream.TDouble)(value)
+    this._t_doubleValue = new (TModel.TDouble)(value)
   }
   set listValue (value) {
-    this._t_listValue = new (TStream.TList(TStream.TString))(value)
+    this._t_listValue = new (TModel.TList(TModel.TString))(value)
   }
   set mapValue (value) {
-    this._t_mapValue = new (TStream.TMap(TStream.TInt32, TStream.TString))(value)
+    this._t_mapValue = new (TModel.TMap(TModel.TInt32, TModel.TString))(value)
   }
   set charValue (value) {
-    this._t_charValue = new (TStream.TInt8)(value)
+    this._t_charValue = new (TModel.TInt8)(value)
   }
   set shortValue (value) {
-    this._t_shortValue = new (TStream.TInt16)(value)
+    this._t_shortValue = new (TModel.TInt16)(value)
   }
   set longlongValue (value) {
-    this._t_longlongValue = new (TStream.TInt64)(value)
+    this._t_longlongValue = new (TModel.TInt64)(value)
   }
   set ucharValue (value) {
-    this._t_ucharValue = new (TStream.TUInt8)(value)
+    this._t_ucharValue = new (TModel.TUInt8)(value)
   }
   set ushortValue (value) {
-    this._t_ushortValue = new (TStream.TUInt16)(value)
+    this._t_ushortValue = new (TModel.TUInt16)(value)
   }
   set uintValue (value) {
-    this._t_uintValue = new (TStream.TUInt32)(value)
+    this._t_uintValue = new (TModel.TUInt32)(value)
   }
   set bytesValue (value) {
-    this._t_bytesValue = new (TStream.TBytes)(value)
+    this._t_bytesValue = new (TModel.TBytes)(value)
   }
 
   writeTo (os, tBuffer) {
